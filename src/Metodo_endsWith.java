34817b2a222c595b800129e501eae4d48fcc62b0
gabyandrade04 <gabrielandrade04@gmail.com>
2017-08-29 16:01:54 -0300

    29/08/2017 Metodos de String,

--- src/Metodo_endsWith.java
+++ src/Metodo_endsWith.java
@@ -11,12 +11,26 @@
 public class Metodo_endsWith {
     
     public static void main(String[] args) {
-        String cadena = "Buenas tardes profe";
+        String cadena = "Buenas Tardes Profe";
         
-        boolean b1 = cadena.endsWith("profe");
-        boolean b2 = cadena.endsWith("noches");
+        boolean b1 = cadena.endsWith("fe");
+        boolean b2 = cadena.endsWith("ofes");
         System.out.println(b1);
         System.out.println(b2);
+        
+        
+        System.out.println(cadena.toLowerCase());//cambia las letras mayusculas en minusculas
+        //System.out.println(cadena.startsWith("b"));
+        System.out.println(cadena.startsWith("B"));// muestra con boolean(true or false) si empieza con la letra
+        
+        System.out.println(cadena.equals("Buena tarde Profe"));//compara la cadena y muetra si es true or false
+        
+        System.out.println(cadena.toUpperCase());// cambia la cedena a mayusculas
+        System.out.println(cadena.replace("a","e")); // reemplaza un letra o palabra por otra
+        System.out.println(cadena.endsWith("ofe"));
+        System.out.println(cadena.indexOf("Tardes"));//retorna -1 cuando no encuentra la palabra, muetra la posicion de la primera letra de la palabra, no distingue mayus o minus
+        System.out.println(cadena.charAt(2));
+        System.out.println(cadena.length());
     }
     
 }
