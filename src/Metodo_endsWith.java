8fcb58a927c2b8a20fe5499e0d84041be6a730bd
gabyandrade04 <gabrielandrade04@gmail.com>
2017-08-29 14:52:06 -0300

    29/08/2017 Metodo String, cadena endsWith

---
+++ src/Metodo_endsWith.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Español
+ */
+public class Metodo_endsWith {
+    
+    public static void main(String[] args) {
+        String cadena = "Buenas tardes profe";
+        
+        boolean b1 = cadena.endsWith("profe");
+        boolean b2 = cadena.endsWith("noches");
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+    
+}
